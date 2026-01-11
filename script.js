a57cbe8267e0642fe8a3d4e21d86cf1b4f56e7f7
Daniil <daniillishankov@gmail.com>
2026-01-12 00:17:45 +0100

    functional buttons added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('feed-button').addEventListener('click', ()=>{
+        document.getElementById('honger-value').innerText = '100';
+});
+
+document.getElementById('sleep-button').addEventListener('click', ()=>{
+    document.getElementById('sleep-value').innerText = '100';
+});
+
+document.getElementById('happiness-button').addEventListener('click', ()=>{
+    document.getElementById('happiness-value').innerText = '100';
+});
